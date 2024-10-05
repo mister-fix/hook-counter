a307c99af4822c3d87d9c65e1c770559d99d759c
Stephen <hellostephenwm@gmail.com>
2024-10-05 13:49:29 -0400

    6-1: employed useReducer hook to manage state of counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const counterReducer = (state, action) => {
+	switch (action.type) {
+		case "INC":
+			return state + 1;
+		case "DEC":
+			return state - 1;
+		case "ZERO":
+			return 0;
+		default:
+			return state;
+	}
+};
 
 const App = () => {
-	return <div>App</div>;
+	const [counter, counterDispatch] = useReducer(counterReducer, 0);
+
+	return (
+		<div>
+			<div>{counter}</div>
+			<div>
+				<button onClick={() => counterDispatch({ type: "INC" })}>+</button>
+				<button onClick={() => counterDispatch({ type: "DEC" })}>-</button>
+				<button onClick={() => counterDispatch({ type: "ZERO" })}>0</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
