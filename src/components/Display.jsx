0582a4f24e8bc57f4a96d93665f7e9b7dbd2a978
Stephen <hellostephenwm@gmail.com>
2024-10-05 15:25:28 -0400

    6-3: moved display component to own file

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import CounterContext from "../CounterContext";
+
+const Display = () => {
+	const [counter, counterDispatch] = useContext(CounterContext);
+
+	return <div>{counter}</div>;
+};
+
+export default Display;
